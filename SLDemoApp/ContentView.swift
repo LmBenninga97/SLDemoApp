a49563d2d423cc091b6ea49238e82e79225de4ab
Lmbenninga97 <lmbenninga@gmail.com>
2023-09-24 10:36:18 +1000

    Initial Commit

---
+++ SLDemoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SLDemoApp
+//
+//  Created by Lino Benninga on 24/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SLDemoApp/SLDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SLDemoAppApp.swift
+//  SLDemoApp
+//
+//  Created by Lino Benninga on 24/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct SLDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
